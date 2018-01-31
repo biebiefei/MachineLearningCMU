90e6e7ab120cda2c79dbaebb0377db264bb03129
何　飛 <fhe@MBP15UAL-NC0213.local>
2018-01-31 11:10:22 +0900

    make derectories

--- Homeworks/make_derectories.sh
+++ Homeworks/make_derectories.sh
@@ -4,7 +4,7 @@ for i in {1..30}
 do
 	dir_name="Lecture"${i}
 	#echo ${dir_name}
-	mkdir ${dir_name}
-	mkdir ${dir_name}/wa
-	mkdir ${dir_name}/bao
+	mkdir ${dir_name} #make lecture directories
+	mkdir ${dir_name}/wa #wawa's directory
+	mkdir ${dir_name}/bao #baobao's directory
 done
